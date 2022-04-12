31cedc230410cd55ae26d9e8e83699fe80a5963b
youranreus <youranreus@qq.com>
2022-04-12 16:38:42 +0800

    refactor: 使用hooks重构Local页面

--- src/pages/Local.js
+++ src/pages/Local.js
@@ -1,112 +1,99 @@
 /**
  * @author 季悠然
- * @date 2021-12-11
+ * @date 2022-04-12
  */
-import React from 'react';
-import {TextArea, Typography, Tag, Space, ButtonGroup, Button, Toast} from '@douyinfe/semi-ui';
-import copy from "copy-to-clipboard";
-import {Redirect, withRouter} from "react-router-dom";
+
+import {Button, ButtonGroup, Space, Tag, TextArea, Toast, Typography} from "@douyinfe/semi-ui";
+import {useHistory, useParams} from "react-router-dom";
 import {IconChevronLeft, IconCopy, IconDelete, IconSave, IconUpload} from "@douyinfe/semi-icons";
+import React, {useEffect, useState} from "react";
+import {copyContent, randomString} from "../utils";
 import axios from "axios";
 import qs from "qs";
 
-class Local extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: props.match.params.id,
-            content: localStorage.getItem(props.match.params.id),
-            deleteVisible: false
-        }
-    }
+function Local() {
+    const {Title} = Typography;
+    const params = useParams()
+    const his = useHistory()
+    const [nid, setNID] = useState(params.id)
+    const [content, setContent] = useState(localStorage.getItem(params.id))
 
-    save = () => {
-        localStorage.setItem(this.state.id, this.state.content);
+    useEffect(() => {
+        if (!localStorage.getItem(params.id)) {
+            Toast.error('好像没有这张便签噢');
+            localStorage.removeItem(params.id);
+            his.push('/')
+        }
+    }, [])
+
+    const save = () => {
+        localStorage.setItem(nid, content);
         Toast.success('保存成功');
     }
 
-    copyContent = () => {
-        copy(this.state.content);
-        Toast.info('复制成功');
+    const quickSave = (e) => {
+        if (e.key === "s" && (e.ctrlKey || e.metaKey)) {
+            e.preventDefault();
+            save();
+        }
     }
 
-    randomString = (s) => {
-        s = s || 32;
-        let t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678",
-            a = t.length,
-            n = "",
-            i = 0;
-        for (; i < s; i++) n += t.charAt(Math.floor(Math.random() * a));
-        return n;
-    }
-
-    upload = ()=>{
-        let newId = this.randomString(6);
-        axios.get('https://i.exia.xyz/note/get/'+newId)
-            .then(data=>{
-                if(data.data.content.id === newId) {
-                    axios.post('https://i.exia.xyz/note/modify/' + newId + '?key=', qs.stringify({content: this.state.content}))
+    const upload = () => {
+        let newId = randomString(6);
+        axios.get('https://i.exia.xyz/note/get/' + newId)
+            .then(data => {
+                if (data.data.content.id === newId) {
+                    axios.post('https://i.exia.xyz/note/modify/' + newId + '?key=', qs.stringify({content}))
                         .then(data => {
                             if (data.data.content === 1 || data.data.content === 0) {
                                 Toast.success('上传成功');
-                                this.props.history.push('/o/' + newId);
+                                his.push('/o/' + newId);
                             } else {
                                 console.log(data);
                                 Toast.error("上传失败");
                             }
                         });
-                }
-                else
+                } else
                     Toast.error("上传失败");
             })
     }
 
-    delete = ()=>{
+    const del = () => {
         let localArr = localStorage.getItem('localArr').split(",");
-        localArr.splice(localArr.indexOf(this.state.id),1);
-        localStorage.removeItem(this.state.id);
+        localArr.splice(localArr.indexOf(this.state.id), 1);
+        localStorage.removeItem(nid);
         localStorage.setItem("localArr", localArr.join(","));
         Toast.success('删除成功');
-        this.props.history.push('/');
+        his.push('/');
     }
 
-    quickSave = (e)=>{
-        if (e.key === "s" && (e.ctrlKey || e.metaKey)) {
-            e.preventDefault();
-            this.save();
-        }
-    }
-
-    render() {
-        const {Title} = Typography;
-        if(!localStorage.getItem(this.props.match.params.id)) {
-            Toast.error('好像没有这张便签噢');
-            localStorage.removeItem(this.props.match.params.id);
-            return (<Redirect to={'/'}/>);
-        }
-
-        return (
-            <div className="local">
-                <div className={"local-header"}>
-                    <Title heading={1}>#{this.state.id}</Title>
-                    <Space>
-                        <Tag size={"large"} color={"blue"}>本地便签</Tag>
-                        <Tag size={"large"} color={"violet"}>len: {this.state.content.length}</Tag>
-                        <Button icon={<IconChevronLeft />} size={"small"} onClick={()=>{this.props.history.push('/');}}/>
-                    </Space>
-                </div>
-                <TextArea rows={30} onKeyDown={this.quickSave} value={this.state.content} onChange={(v)=>this.setState({content: v})}/>
-                <div style={{textAlign: "right", marginTop: "1rem"}}>
-                    <ButtonGroup>
-                        <Button onClick={this.save} icon={<IconSave />}/>
-                        <Button onClick={this.copyContent} icon={<IconCopy />}/>
-                        <Button onClick={this.upload} icon={<IconUpload />}/>
-                        <Button type={"danger"} onClick={this.delete} icon={<IconDelete />}/>
-                    </ButtonGroup>
-                </div>
+    return (
+        <div className="local">
+            <div className={"local-header"}>
+                <Title heading={1}>#{nid}</Title>
+                <Space>
+                    <Tag size={"large"} color={"blue"}>本地便签</Tag>
+                    <Tag size={"large"} color={"violet"}>len: {content.length}</Tag>
+                    <Button icon={<IconChevronLeft/>} size={"small"} onClick={() => {
+                        his.push('/');
+                    }}/>
+                </Space>
             </div>
-        );
-    }
+            <TextArea rows={30} onKeyDown={quickSave} value={content} onChange={(v) => {
+                setContent(v)
+            }}/>
+            <div style={{textAlign: "right", marginTop: "1rem"}}>
+                <ButtonGroup>
+                    <Button onClick={save} icon={<IconSave/>}/>
+                    <Button onClick={() => {
+                        copyContent(content)
+                    }} icon={<IconCopy/>}/>
+                    <Button onClick={upload} icon={<IconUpload/>}/>
+                    <Button type={"danger"} onClick={del} icon={<IconDelete/>}/>
+                </ButtonGroup>
+            </div>
+        </div>
+    );
 }
 
-export default withRouter(Local);
+export default Local
