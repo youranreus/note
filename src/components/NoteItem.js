71aedd882862f1f9161bb9a274059bfecb8d1441
youranreus <youranreus@qq.com>
2022-04-12 13:15:30 +0800

    feat: NoteItem组件封装

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -3,23 +3,47 @@
  * @date 2022-04-11
  */
 
-import {useState} from "react";
-import { Card } from '@douyinfe/semi-ui';
+import {useEffect, useState} from "react";
+import {Card} from '@douyinfe/semi-ui';
+import axios from "axios";
+import {useHistory} from "react-router-dom";
 
 function NoteItem(props) {
     const {type, nid} = props
-    const { Meta } = Card
+    const {Meta} = Card
+    const his = useHistory()
+    const [data, setData] = useState('加载中')
+    const [loading, setLoading] = useState(true)
 
-    const [loading, setLoading] = useState(true);
+    const getData = () => {
+        if (type === 'local') {
+            setData(localStorage.getItem(nid))
+            setLoading(false)
+        } else {
+            axios.get('https://i.exia.xyz/note/get/' + nid)
+                .then(res => {
+                    setData(res.data.content.content)
+                    setLoading(false)
+                })
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const jump = () => {
+        his.push('/' + (type === 'online' ? 'o/' : 'l/') + nid)
+    }
 
     return (
         <Card
-            loading={ loading }
+            loading={loading} className={"item"}
         >
             <Meta
-                title="Semi Doc"
-                description="全面、易用、优质"
+                title={nid}
             />
+            <p onClick={jump}>{data}</p>
         </Card>
     );
 }
