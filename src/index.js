d3b1c8e182dd689f356abe82fa19722b35fdf4ad
youranreus <youranreus@qq.com>
2023-10-23 12:12:44 +0800

    feat: 依赖升级

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'normalize.css/normalize.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'normalize.css/normalize.css'
 
-ReactDOM.render(
-  //<React.StrictMode>
-    <App />,
-  //</React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
