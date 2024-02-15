f25fcd5c6d0c5dcc834038a4303d299176ac79ef
季悠然 <youranreus@qq.com>
2024-02-15 11:25:12 +0800

    feat: 基础配置完善

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  title: 'Memo',
+  theme: {
+    dark: false,
+    colors: {
+      primary: '#ff0000',
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="tw-h-5 tw-text-center">
+    haha
+    <n-button>eee</n-button>
+  </div>
+</template>
